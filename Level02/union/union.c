96a0db91fa58c5f2b4e7897d0f35bccda8b0061c
MaryamPazouki <mpazouki@student.42wolfsburg.de>
2024-10-06 20:49:45 +0000

    union

--- Level02/union/union.c
+++ Level02/union/union.c
@@ -5,13 +5,13 @@ int main(int argc, char **argv)
     int i;
     i = 0;
 
-    char seen[256] = {0};
+    unsigned char seen[256] = {0};
 
     if (argc == 3)
     {
         while (argv[1][i])
         {       
-            if (!seen[argv[1][i]] == 1)
+            if (!seen[(unsigned char )argv[1][i]])
             {    
                 write(1, &argv[1][i], 1);
                 seen[argv[1][i]] = 1;
@@ -21,7 +21,7 @@ int main(int argc, char **argv)
         i = 0;
         while (argv[2][i])
         {
-            if (!seen[argv[2][i]] == 1)
+            if (!seen[(unsigned char )argv[2][i]])
             {    
                 write(1, &argv[2][i], 1);
                 seen[argv[2][i]] = 1;
