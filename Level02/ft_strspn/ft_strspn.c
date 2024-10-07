b4585324ac4ef30b7389dffb28c1a5b61be20347
MaryamPazouki <mpazouki@student.42wolfsburg.de>
2024-10-07 20:35:51 +0000

    ft_spn

--- Level02/ft_strspn/ft_strspn.c
+++ Level02/ft_strspn/ft_strspn.c
@@ -1,5 +1,19 @@
 #include<string.h>
 
+char *ft_strchr(const char *s, int c)
+{
+    while(*s != '\0')
+    {
+        if (*s == c)
+        {
+            return((char *) s );
+        }
+        s++;
+    }
+    return(0);
+}
+
+
 size_t	ft_strspn(const char *s, const char *accept)
 {
     int i, j;
@@ -7,9 +21,21 @@ size_t	ft_strspn(const char *s, const char *accept)
     
     while (s[i] != '\0')        
     {
-        while (s[i] == accept[j])
-            j++;
+        if (ft_strchr(accept, s[i]) == 0)
+            break;
         i++;
     }
-    return(j);
+    return(i);
+}
+
+#include<stdio.h>
+
+int main()
+{
+    char *s = "abcde1234ef";
+    char *accept = "abcd12";
+
+    printf("the ft_strspn output %ld \n ", ft_strspn(s,accept));
+    printf("the ft_strspn output %ld \n",  strspn(s,accept));
+
 }
