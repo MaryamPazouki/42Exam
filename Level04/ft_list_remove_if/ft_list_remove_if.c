1034ba04bf42d9e29a6b833fcfb0c339175cc86e
MaryamPazouki <mpazouki@student.42wolfsburg.de>
2024-11-04 11:33:59 +0000

    remove if

--- Level04/ft_list_remove_if/ft_list_remove_if.c
+++ Level04/ft_list_remove_if/ft_list_remove_if.c
@@ -3,19 +3,21 @@
 
 void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 {
+    
+    if (begin_list == NULL || *begin_list == NULL)
+        return;
+    
     t_list *cur;
     cur  = *begin_list;
 
-    while(begin_list)
-    {
-        if (cmp(cur->data, data_ref) == 0)
+    if (cmp(cur->data, data_ref) == 0)
         {
             *begin_list = cur ->next;
             fre(cur);
             ft_list_remove_if(begin_list, data_ref, cmp);
         }
-        else
-        {
+    else
+         {
             cur = *begin_list;
              ft_list_remove_if(&cur -> next, data_ref, cmp);
         }
