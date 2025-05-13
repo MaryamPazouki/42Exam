30a171e8629940099787274420008f9ff6f2bf93
MaryamPazouki <mpazouki@student.42wolfsburg.de>
2025-05-13 07:15:43 +0000

    Rank4

--- Rank4/microshell.c
+++ Rank4/microshell.c
@@ -1,19 +1,113 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   miroschell.c                                       :+:      :+:    :+:   */
+/*   microschell.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mpazouki <mpazouki@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/08 13:45:15 by mpazouki          #+#    #+#             */
-/*   Updated: 2025/05/08 13:45:15 by mpazouki         ###   ########.fr       */
+/*   Created: 2025/05/11 23:48:06 by mpazouki          #+#    #+#             */
+/*   Updated: 2025/05/11 23:48:06 by mpazouki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 
+void err_fatal(void)
+{
+    write(2, "Err: fatal\n", 12);
+    exit(1);
+}
+
+void err_exec(char *cmd)
+{
+	write(2, "error: cannot execute ", 23);
+	write(2, cmd, strlen(cmd));
+	write(2, "\n", 1);
+	exit(1);
+}
+
+
+
+int ft_cd(char **args)
+{
+	int count = 0;
+	while (args[count])
+		count++;
+	if (count != 2)
+	{
+		write(2, "error: cd: bad arguments\n", 25);
+		return (1);
+	}
+	if (chdir(args[1]) == -1)
+	{
+		write(2, "error: cd: cannot change directory to ", 38);
+		write(2, args[1], strlen(args[1]));
+		write(2, "\n", 1);
+		return (1);
+	}
+	return (0);
+}
+
+int main(int argc, char **argv, char **envp)
+{
+	int i = 1;
+	int start, j;
+
+	while (i < argc)
+	{
+		start = i;
+		while (i < argc && strcmp(argv[i], "|") && strcmp(argv[i], ";"))
+			i++;
+
+		if (i > start)
+		{
+			// Create null-terminated command array
+			char **cmd = malloc(sizeof(char *) * (i - start + 1));
+			if (!cmd)
+				err_fatal();
+
+			for (j = 0; j < i - start; j++)
+				cmd[j] = argv[start + j];
+			cmd[j] = NULL;
+
+			// Built-in cd
+			if (!strcmp(cmd[0], "cd"))
+				ft_cd(cmd);
+			else
+			{
+				pid_t pid = fork();
+				if (pid < 0)
+					err_fatal();
+				else if (pid == 0)
+				{
+					execve(cmd[0], cmd, envp);
+					err_exec(cmd[0]);
+				}
+				else
+				{
+					if (waitpid(pid, NULL, 0) == -1)
+						err_fatal();
+				}
+			}
+			free(cmd);
+		}
+
+		// Skip over ";" or "|"
+		if (i < argc && (!strcmp(argv[i], ";") || !strcmp(argv[i], "|")))
+			i++;
+	}
+	return (0);
+}
+
+
+
+
+
+/*
 int main(int argc, char **argv, char **envp)
 {
     
@@ -25,24 +119,36 @@ int main(int argc, char **argv, char **envp)
      while(i< argc)
      {
         start = i;
-        while(i<argc && (strcmp(argv[i], "|") && strcmp(argv[i], ";") ))
+        while(i < argc && (strcmp(argv[i], "|") && strcmp(argv[i], ";") ))
             i++;
         if (i > start)
         {
-            j=start;
-            write(1, "command", 9);
-            while(j < i)
-            {
-                write(1, argv[i], strlen(argv[i]));
-                if (j + 1 < i)
-                    write(1, " ", 1);
-                j++;
-            }
-            write(1, "\n", 1);
+           if (!strcmp(argv[start], "cd"))
+           {
+             argv[i] = NULL;
+             ft_cd(&argv[start]);
+           }
+           else 
+           {        
+                argv[i] = NULL;
+                pid_t pid = fork();
+                if (pid < 0)
+                    err_fatal();
+                else if(pid == 0)
+                {
+                    execve(argv[start], &argv[start], envp);
+                    err_exec();
+                }
+                else 
+                {
+                    if (waitpid (pid , NULL, 0)== -1)
+                        err_fatal();
+                }
+           }
         }
-        if (i<argc &&  (!strcmp(argv[i], "|") || !strcmp(argv[i],";")))
+        if (i < argc &&  (!strcmp(argv[i], "|") || !strcmp(argv[i], ";")))
             i++;
     }
     (void )envp;
     return 0;
-}
+}*/
