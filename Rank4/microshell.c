b6e8344903334557d5419c39ac54e4339089d92a
MaryamPazouki <mpazouki@student.42wolfsburg.de>
2025-05-17 23:58:58 +0000

    final with command

--- Rank4/microshell.c
+++ Rank4/microshell.c
@@ -15,12 +15,14 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 
+// Prints fatal error and exits
 void err_fatal(void)
 {
     write(2, "error: fatal\n", 13);
     exit(1);
 }
 
+// Prints exec error if execve fails
 void err_exec(char *cmd)
 {
     write(2, "error: cannot execute ", 23);
@@ -29,16 +31,19 @@ void err_exec(char *cmd)
     exit(1);
 }
 
+// Built-in cd implementation with argument and error checks
 int ft_cd(char **args)
 {
     int count = 0;
     while (args[count])
         count++;
+    // cd must be followed by exactly one argument
     if (count != 2)
     {
         write(2, "error: cd: bad arguments\n", 25);
         return (1);
     }
+    // Attempt to change directory
     if (chdir(args[1]) == -1)
     {
         write(2, "error: cd: cannot change directory to ", 38);
@@ -49,9 +54,12 @@ int ft_cd(char **args)
     return (0);
 }
 
+// Executes a command with optional piping
 int execute(char **argv, int i, char **envp, int input_fd, int has_pipe)
 {
     int fd[2];
+
+    // Create a pipe if needed
     if (has_pipe && pipe(fd) == -1)
         err_fatal();
 
@@ -60,10 +68,17 @@ int execute(char **argv, int i, char **envp, int input_fd, int has_pipe)
         err_fatal();
     else if (pid == 0)
     {
+        // In child
+
+        // Redirect stdin if input_fd was passed (i.e., reading from previous pipe)
         if (input_fd != 0 && dup2(input_fd, 0) == -1)
             err_fatal();
+
+        // Redirect stdout to write end of pipe
         if (has_pipe && dup2(fd[1], 1) == -1)
             err_fatal();
+
+        // Close file descriptors
         if (input_fd)
             close(input_fd);
         if (has_pipe)
@@ -71,18 +86,30 @@ int execute(char **argv, int i, char **envp, int input_fd, int has_pipe)
             close(fd[0]);
             close(fd[1]);
         }
+
+        // Execute the command
         execve(argv[0], argv, envp);
+        // If execve fails
         err_exec(argv[0]);
     }
     else
     {
+        // In parent
+
+        // Wait for the child if it's the last command or part of sequential execution
         if (waitpid(pid, NULL, 0) == -1)
             err_fatal();
+
+        // Close input_fd if it was opened
         if (input_fd)
             close(input_fd);
+
+        // If there's a pipe, return the read end to be used in the next command
         if (has_pipe)
-            close(fd[1]);
-        return has_pipe ? fd[0] : 0;
+        {
+            close(fd[1]);     // Close unused write end
+            return fd[0];     // Return read end for next input
+        }
     }
     return 0;
 }
@@ -90,40 +117,55 @@ int execute(char **argv, int i, char **envp, int input_fd, int has_pipe)
 int main(int argc, char **argv, char **envp)
 {
     int i = 1, start, j;
-    int input_fd = 0;
+    int input_fd = 0;  // Will be used for chaining pipe inputs
 
     while (i < argc)
     {
         start = i;
+        // Move i to the next separator '|' or ';'
         while (i < argc && strcmp(argv[i], "|") && strcmp(argv[i], ";"))
             i++;
 
         if (i > start)
         {
+            // Build the command array
             char **cmd = malloc(sizeof(char *) * (i - start + 1));
             if (!cmd)
                 err_fatal();
-            for (j = 0; j < i - start; j++)
+			j = 0;
+			while(j < i -start)
+			{
                 cmd[j] = argv[start + j];
-            cmd[j] = NULL;
+				j++;
+			}
+			cmd[j] = NULL;
 
+            // Check if the next token is a pipe
             int is_pipe = (i < argc && !strcmp(argv[i], "|"));
+
+            // Handle built-in cd
             if (!strcmp(cmd[0], "cd"))
             {
+                // cd must not be part of a pipeline
                 if (input_fd)
                     close(input_fd);
                 ft_cd(cmd);
-                input_fd = 0;
+                input_fd = 0;  // Reset input
             }
             else
+            {
+                // Execute normal command with pipe if needed
                 input_fd = execute(cmd, i - start, envp, input_fd, is_pipe);
+            }
 
             free(cmd);
         }
 
+        // Move past the separator token if present
         if (i < argc && (!strcmp(argv[i], ";") || !strcmp(argv[i], "|")))
             i++;
     }
     return 0;
 }
+
 
