ad15a6b970833867decc5b481dc85e85747351ae
MaryamPazouki <maryam.pazoki@gmail.com>
2026-02-24 13:44:33 +0100

    Add files via upload

--- Rank5/Level_2/BSQ/main.c
+++ Rank5/Level_2/BSQ/main.c
@@ -1,47 +1,21 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mpazouki <mpazouki@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/20 12:26:42 by mpazouki          #+#    #+#             */
-/*   Updated: 2026/02/24 12:01:10 by mpazouki         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "bsq.h"
 
-static void process_map(const char *filename)
-{
-    t_map map = {0};
-    
-    if (!parse_map(filename, &map))
-    {
-        fputs("map error \n", stderr);
-        return;
+int main(int ac, char **av) {
+    FILE *file;
+
+    if (ac == 1) {
+        bsq(stdin);
+    } else {
+        for (int i = 1; i < ac; i++) {
+            file = fopen(av[i], "r");
+            if (file) {
+                bsq(file);
+                fclose(file);
+            } else {
+                fprintf(stderr, "map error\n");
+            }
+        }
     }
 
-    solve_bsq(&map);
-    print_map(&map);
-    free_map(&map);
-}
-
-int main(int argc, char **argv)
-{
-    if (argc == 1)
-    {
-        process_map(NULL);
-        return 0;
-    }
-    fputs("map goes to  open \n", stderr);
-    for (int i = 0; i < argc ; i++)
-    {
-        process_map(argv[i]);
-        
-        // Print newline between maps (but not after the last one)
-        if (i< argc -1)
-            fputc('\n', stdout);
-    }
     return 0;
 }
